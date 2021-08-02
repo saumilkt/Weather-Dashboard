104f5fd3d4faf32c75d37c6854ce9ac9086a9427
saumilkt <saumilkt@gmail.com>
2021-08-01 22:23:30 -0700

    creating intial elements and containers

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+// Get form element value
+let leftColumnEL = document.querySelector("#left-column")
+
+// Get all the elements of cities list for event handler
+let citiesListContainerBtnEl = document.querySelector(".list-group-item");
+// Daily forcast Containter
+let dailyWeatherContainerEl = document.querySelector("#forecast-output-container"); 
+
+// Create a form container and containing elements
+let dynFormContainer = document.createElement("form");
+dynFormContainer.setAttribute("id", "dymCityForm");
+dynFormContainer.classList = "city-search-forecast-container";
+leftColumnEL.appendChild(dynFormContainer)
+
+
+// Create H3 element
+let formH3 = document.createElement("h3");
+formH3.textContent = " Search for a City ";
+dynFormContainer.appendChild(formH3);
+
+// Create input element
+let formInput = document.createElement("input");
+formInput.setAttribute("id", "city-name")
+formInput.setAttribute("type", "text");
+formInput.setAttribute("autofocus", "true");
+formInput.classList = "form-input";
+dynFormContainer.appendChild(formInput);
+
+// Create button element
+let formButton = document.createElement("button");
+formButton.setAttribute("type", "submit");
+formButton.classList= ("btn fas fa-search");
+dynFormContainer.appendChild(formButton);
+
+// Find the city form
+let seachEventHanglerEl = document.querySelector("#dymCityForm");
+let searchByCityEl = document.querySelector("#city-name");
+
+
+// Left column cities container
+let citiesContainerEl = document.createElement("div");
+citiesContainerEl.setAttribute("id", "dym-cities-list");
+citiesContainerEl.classList = "list-group";
+
+// Append to the left column
+leftColumnEL.appendChild(citiesContainerEl);
+
+// Find the list div container
+let citiesListContainerEl = document.querySelector("#dym-cities-list");
