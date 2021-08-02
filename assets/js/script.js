b66fdd2a93953a63488fbceb01215efde71cde5a
saumilkt <saumilkt@gmail.com>
2021-08-01 22:35:04 -0700

    added function to populate current and 5day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,3 +127,131 @@ function fetchSecondCall(searchByCity, latNum, lonNum, unixTimeCurrentDay, curre
         populate5DayForecast(secondCallData)
     });
 };
+
+// Function to populate current day forecast
+function populateCurrentDayHtml(searchByCity, fullDayDaily, currentDayIcon, currentTempImperial, currentHumidity, currentMPS, mphWindSpeed, uvIndex) {
+    // Populate current Day html elements
+    let dailyForecastContainerEl = document.createElement("div");
+    dailyForecastContainerEl.setAttribute("id", "daily-forecast-container");
+    dailyForecastContainerEl.classList = "borderDiv";
+
+    let currentDayTitle = document.createElement("h3");
+    currentDayTitle.textContent = ( searchByCity.charAt(0).toUpperCase() + searchByCity.slice(1) + " " + fullDayDaily);
+
+    let currentIconEl = document.createElement("span")
+   // "<i class='fas fa-check-square status-icon icon-success'></i>"
+    let currentIconSymbol = "http://openweathermap.org/img/wn/" + currentDayIcon + "@2x.png";
+   // alert(currentIconSymbol);
+   currentIconEl.innerHTML = "<img src=" + currentIconSymbol + "></img>";
+   currentDayTitle.append(currentIconEl)
+
+    // Create p elements to hold the rest of current day informatino
+    let currentTempEl = document.createElement("p");
+    let currentHumidityEl = document.createElement("p");
+    let currentWinSpEl = document.createElement("p");
+    let currentUvIEl = document.createElement("p");
+
+    // Assign helments text content
+    // Round temperature to no decimal places
+    currentTempEl.textContent = "Temperature: " + (currentTempImperial.toFixed(1)) + " °F";
+    currentHumidityEl.textContent = "Humidity: " + currentHumidity + "%";
+    currentWinSpEl.textContent = "Wind Speed: " + currentMPS + " MPH";
+    currentUvIEl.textContent = "UV Index: " + uvIndex;
+
+    $("#daily-forecast-container").remove(); // Remove all list items from the document with jquery
+
+    // *** Append to forecast output container
+    // Append daily forecast
+    dailyWeatherContainerEl.appendChild(dailyForecastContainerEl);
+    dailyForecastContainerEl.appendChild(currentDayTitle);
+    dailyForecastContainerEl.appendChild(currentTempEl);
+    dailyForecastContainerEl.appendChild(currentHumidityEl);
+    dailyForecastContainerEl.appendChild(currentWinSpEl);
+    dailyForecastContainerEl.appendChild(currentUvIEl);
+};
+
+  
+function populate5DayForecast(secondCallData) {
+    
+    $("#weekly-forecast-container").remove(); // Remove all list items from the document with jquery
+
+    // Populate current Day html elements
+    let weeklyForecastContainerEl = document.createElement("div");
+    weeklyForecastContainerEl.setAttribute("id", "weekly-forecast-container");
+    //weeklyForecastContainerEl.classList = "borderDiv";
+    weeklyForecastContainerEl.classList = "border-Div-right-column"; 
+
+    let fiveDayForecast = document.createElement("h3");
+    fiveDayForecast.textContent = "5-Day Forecast:"
+
+    // Append as topmost before for loop to generate contents of each div container.
+    dailyWeatherContainerEl.appendChild(weeklyForecastContainerEl);
+    weeklyForecastContainerEl.appendChild(fiveDayForecast);
+
+    // Create a div just to hold the 5 day as a flex row 
+    let weeklyFlexContainerEL = document.createElement("div");
+    weeklyFlexContainerEL.classList = "weekly-flex-conatiner"
+
+    // Append only after the date on the 5 Day Forecast
+    weeklyForecastContainerEl.appendChild(weeklyFlexContainerEL);
+
+    for (i=1; i <= 5; i++) { // Get 5 days worth of conent from the 5 day forecast.
+        let unixTime = secondCallData.daily[i].dt;
+        //console.log("Correct 5 day forcast" + unixTime)
+    
+        let unix_timestamp = unixTime;
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp * 1000);
+        // Hours part from the timestamp
+        var year = date.getFullYear();
+        var monthOfYear = date.getMonth() + 1;
+        var dayOfMonth = date.getDate();
+    
+        // Values to be displayed
+        var fullDay = (date.getMonth() + 1) + "/" + date.getDate() + "/"  + date.getFullYear(); // Date
+        var iconWeather = secondCallData.daily[i].weather[0].icon // icon
+        let fahrenheitTemp = secondCallData.daily[i].temp.day // Temp @ fahrenheit
+        let humidity = secondCallData.daily[i].humidity;
+        //console.log(fullDay)
+        //console.log(iconWeather)
+        //console.log("Temp: " + fahrenheitTemp.toFixed(1) + " °F"); // Fahrenheit temperature
+        //console.log("Temp: " + fahrenheitTemp + " °F")
+        //console.log("Humidity: " + humidity);
+    
+        // *** Create 5 Day elements and display them on screen.
+        // Create a div to hold each day of the 5 day weekly forecast.
+        let eachDayContainer = document.createElement("div");
+        eachDayContainer.setAttribute("id", ("day=" + [i]));
+        eachDayContainer.classList = "border-div-five-day-forecast";
+       
+        let currentDayTitle = document.createElement("p");
+        currentDayTitle.textContent = (fullDay);
+
+        // Span to hold the icon
+        let iconSpan = document.createElement("p");
+        iconSpan.textContent = "";
+
+        let currentIconEl = document.createElement("span")
+        let currentIconSymbol = "http://openweathermap.org/img/wn/" + iconWeather + "@2x.png";
+        // alert(currentIconSymbol);
+        currentIconEl.innerHTML = "<img src=" + currentIconSymbol + "></img>";
+        iconSpan.append(currentIconEl)
+
+        // Create p elements to hold the rest of current day informatino
+        let currentTempEl = document.createElement("p");
+        let currentHumidityEl = document.createElement("p");
+        
+        currentTempEl.textContent = "Temperature: " +  (fahrenheitTemp.toFixed(2)) + " °F";
+        currentHumidityEl.textContent = "Humidity: " + humidity + "%";
+          
+        // *** Append to forecast output container
+        // Append daily forecast
+        eachDayContainer.appendChild(currentDayTitle);
+        eachDayContainer.appendChild(currentIconEl);
+        eachDayContainer.appendChild(currentTempEl);
+        eachDayContainer.appendChild(currentHumidityEl);
+        // Once all items have been appended to the eachDayContainer we can append to the parent.
+        weeklyFlexContainerEL.appendChild(eachDayContainer);
+    };
+};
